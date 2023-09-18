5ed66cbaf5172f8fb2fb5f052ce669c435bec264
Martijn de Vos <mdmartijndevos@gmail.com>
2023-09-18 17:23:33 -0500

    adding in devos50's SHA1 engine

--- include/hw/misc/s5l8702-sha.h
+++ include/hw/misc/s5l8702-sha.h
@@ -3,6 +3,7 @@
 
 #include "qom/object.h"
 #include "hw/sysbus.h"
+#include <openssl/sha.h>
 
 #define TYPE_S5L8702_SHA    "s5l8702-sha"
 OBJECT_DECLARE_SIMPLE_TYPE(S5L8702ShaState, S5L8702_SHA)
@@ -12,13 +13,32 @@ OBJECT_DECLARE_SIMPLE_TYPE(S5L8702ShaState, S5L8702_SHA)
 
 #define S5L8702_SHA_NUM_REGS    (S5L8702_SHA_SIZE / sizeof(uint32_t))
 
+#define SHA1_BUFFER_SIZE 1024 * 1024
+
+#define SHA_CONFIG         0x0
+#define SHA_RESET          0x4
+#define SHA_MEMORY_MODE    0x80 // whether we read from the memory
+#define SHA_MEMORY_START   0x84
+#define SHA_INSIZE         0x8c
+
 struct S5L8702ShaState {
     /*< private >*/
     SysBusDevice parent_obj;
 
     /*< public >*/
     MemoryRegion iomem;
-    uint32_t regs[S5L8702_SHA_NUM_REGS];
+
+    uint32_t config;
+    uint32_t memory_start;
+    uint32_t memory_mode;
+    uint32_t insize;
+    uint8_t buffer[SHA1_BUFFER_SIZE];
+    uint32_t hw_buffer[0x10]; // hardware buffer
+    uint32_t buffer_ind;
+    uint8_t hashout[0x14];
+    bool hw_buffer_dirty;
+    bool hash_computed;
+    SHA_CTX ctx;
 };
 
 #endif /* HW_MISC_S5L8702_SHA_H */
