8bade7d9fa4d04828ed3608cdf3e4e2a6085b3a7
Iscle <albertiscle9@gmail.com>
2023-09-20 12:01:22 +0200

    Fixed SHA peripheral
    Before, the SHA peripheral did not reset unless the "reset" register (or what we think is the reset register) at offset 0x0004 was written. However, at least the iPod Classic implementation does not make use of this, and instead writes 0x2 to the config register when starting a new SHA calculation.
    
    We now use this to know when to start over and clear the internal buffer.
    
    Furthermore, some leftovers from the iPod Touch port that did not apply to the S5L8702 were removed.

--- hw/misc/s5l8702-sha.c
+++ hw/misc/s5l8702-sha.c
@@ -4,146 +4,112 @@
 #include "qemu/module.h"
 #include "hw/misc/s5l8702-sha.h"
 
-#define REG_INDEX(offset) (offset / sizeof(uint32_t))
+#define SHA1CONFIG  0x0000
+#define SHA1RESET   0x0004
+#define SHA1OUT     0x0020
+#define SHA1IN      0x0040
 
 static uint64_t swapLong(void *X) {
     uint64_t x = (uint64_t) X;
     x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
     x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
-    x = (x & 0x00FF00FF00FF00FF) << 8  | (x & 0xFF00FF00FF00FF00) >> 8;
+    x = (x & 0x00FF00FF00FF00FF) << 8 | (x & 0xFF00FF00FF00FF00) >> 8;
     return x;
 }
 
-static void flush_hw_buffer(S5L8702ShaState *s) {
-    // Flush the hardware buffer to the state buffer and clear the buffer.
-    memcpy(s->buffer + s->buffer_ind, (uint8_t *)s->hw_buffer, 0x40);
-    memset(s->hw_buffer, 0, 0x40);
-    s->hw_buffer_dirty = false;
-    s->buffer_ind += 0x40;
-}
-
 static void sha1_reset(S5L8702ShaState *s) {
-	s->config = 0;
-	s->memory_start = 0;
-	s->memory_mode = 0;
-	s->insize = 0;
-	memset(&s->buffer, 0, SHA1_BUFFER_SIZE);
-	memset(&s->hw_buffer, 0, 0x10 * sizeof(uint32_t));
-	s->buffer_ind = 0;
-	memset(&s->hashout, 0, 0x14);
-	s->hw_buffer_dirty = false;
-	s->hash_computed = false;
+    memset(s->buffer, 0, sizeof(s->buffer));
+    s->buffer_len = 0;
+    memset(s->outbuf, 0, sizeof(s->outbuf));
+    s->hash_computed = false;
 }
 
 static uint64_t s5l8702_sha_read(void *opaque, hwaddr offset,
-                                      unsigned size)
-{
+                                 unsigned size) {
     S5L8702ShaState *s = S5L8702_SHA(opaque);
-    
-    switch(offset) {
-		case SHA_CONFIG:
-			return s->config;
-		case SHA_RESET:
-			return 0;
-		case SHA_MEMORY_START:
-			return s->memory_start;
-		case SHA_MEMORY_MODE:
-			return s->memory_mode;
-		case SHA_INSIZE:
-			return s->insize;
-		/* Hash result ouput */
-		case 0x20 ... 0x34:
-			//fprintf(stderr, "Hash out %08x\n",  *(uint32_t *)&s->hashout[offset - 0x20]);
-            if(!s->hash_computed) {
+
+    switch (offset) {
+        case SHA_CONFIG:
+            return s->config;
+        case SHA_RESET:
+            return 0;
+        case SHA1OUT ... SHA1OUT + 16 * 4:
+            if (!s->hash_computed) {
                 // lazy compute the final hash by inspecting the last eight bytes of the buffer, which contains the length of the input data.
-                uint64_t data_length = swapLong(((uint64_t *)s->buffer)[s->buffer_ind / 8 - 1]) / 8;
+                uint64_t data_length = swapLong(((uint64_t *) s->buffer)[s->buffer_len / 8 - 1]) / 8;
+                printf("SHA1: data length %d\n", data_length);
 
                 SHA_CTX ctx;
                 SHA1_Init(&ctx);
                 SHA1_Update(&ctx, s->buffer, data_length);
-                SHA1_Final(s->hashout, &ctx);
+                SHA1_Final(s->outbuf, &ctx);
                 s->hash_computed = true;
             }
 
-			return *(uint32_t *)&s->hashout[offset - 0x20];
-	}
+            return *(uint32_t *) &s->outbuf[offset - SHA1OUT];
+        default:
+            qemu_log_mask(LOG_UNIMP, "%s: unimplemented read (offset 0x%04x)\n",
+                          __func__, (uint32_t) offset);
+    }
 
     return 0;
 }
 
 static void s5l8702_sha_write(void *opaque, hwaddr offset,
-                                   uint64_t value, unsigned size)
-{
+                              uint64_t val, unsigned size) {
     S5L8702ShaState *s = S5L8702_SHA(opaque);
-    const uint32_t idx = REG_INDEX(offset);
 
-    switch(offset) {
-		case SHA_CONFIG:
-			if(value == 0x2 || value == 0xa)
-			{
-                if(s->hw_buffer_dirty) {
-                    flush_hw_buffer(s);
+    switch (offset) {
+        case SHA_CONFIG:
+            printf("SHA config: %08x\n", val);
+            if (val == 0x2 || val == 0xa) {
+                if (val == 0x2) {
+                    sha1_reset(s);
                 }
 
-				if(s->memory_mode)
-				{
-					// we are in memory mode - gradually add the memory to the buffer
-					for(int i = 0; i < s->insize / 0x40; i++) {
-						cpu_physical_memory_read(s->memory_start + i * 0x40, s->buffer + s->buffer_ind, 0x40);
-						s->buffer_ind += 0x40;
-					}
-				}
-			} else {
-				s->config = value;
-			}
-			break;
-		case SHA_RESET:
-			sha1_reset(s);
-			break;
-		case SHA_MEMORY_START:
-			s->memory_start = value;
-			break;
-		case SHA_MEMORY_MODE:
-			s->memory_mode = value;
-			break;
-		case SHA_INSIZE:
-            assert(value <= SHA1_BUFFER_SIZE);
-			s->insize = value;
-			break;
-		case 0x40 ... 0x7c:
-            // write to the hardware buffer
-            s->hw_buffer[(offset - 0x40) / 4] |= value;
-            s->hw_buffer_dirty = true;
-			break;
-	}
+                memcpy(s->buffer + s->buffer_len, s->inbuf, sizeof(s->inbuf));
+                s->buffer_len += sizeof(s->inbuf);
+
+                printf("Current length: %d\n", s->buffer_len);
+
+                memset(s->inbuf, 0, sizeof(s->inbuf));
+
+                s->hash_computed = false;
+            } else {
+                s->config = val;
+            }
+            break;
+        case SHA_RESET:
+            printf("SHA reset: %08x\n", val);
+            sha1_reset(s);
+            break;
+        case SHA1IN ... SHA1IN + 16 * 4:
+            printf("SHA hw buffer[%d]: %08x\n", (offset - SHA1IN) / 4, val);
+            s->inbuf[(offset - SHA1IN) / 4] = val;
+            break;
+        default:
+            qemu_log_mask(LOG_UNIMP, "%s: unimplemented write (offset 0x%04x)\n",
+                          __func__, (uint32_t) offset);
+    }
 }
 
 static const MemoryRegionOps s5l8702_sha_ops = {
-    .read = s5l8702_sha_read,
-    .write = s5l8702_sha_write,
-    .endianness = DEVICE_NATIVE_ENDIAN,
-    // .valid = {
-    //     .min_access_size = 4,
-    //     .max_access_size = 4,
-    // },
-    // .impl.min_access_size = 4,
+        .read = s5l8702_sha_read,
+        .write = s5l8702_sha_write,
+        .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
-static void s5l8702_sha_reset(DeviceState *dev)
-{
+static void s5l8702_sha_reset(DeviceState *dev) {
     S5L8702ShaState *s = S5L8702_SHA(dev);
 
     printf("s5l8702_sha_reset\n");
 
-    /* Reset registers */
-    // memset(s->regs, 0, sizeof(s->regs));
-
-    /* Set default values for registers */
-
+    s->config = 0;
+    memset(s->inbuf, 0, sizeof(s->inbuf));
+    sha1_reset(s);
 }
 
-static void s5l8702_sha_init(Object *obj)
-{
+static void s5l8702_sha_init(Object *obj) {
     S5L8702ShaState *s = S5L8702_SHA(obj);
 
     printf("s5l8702_sha_init\n");
@@ -153,20 +119,19 @@ static void s5l8702_sha_init(Object *obj)
     sysbus_init_mmio(SYS_BUS_DEVICE(obj), &s->iomem);
 }
 
-static void s5l8702_sha_class_init(ObjectClass *klass, void *data)
-{
+static void s5l8702_sha_class_init(ObjectClass *klass, void *data) {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
     dc->reset = s5l8702_sha_reset;
 }
 
 static const TypeInfo s5l8702_sha_types[] = {
-    {
-        .name = TYPE_S5L8702_SHA,
-        .parent = TYPE_SYS_BUS_DEVICE,
-        .instance_init = s5l8702_sha_init,
-        .instance_size = sizeof(S5L8702ShaState),
-        .class_init = s5l8702_sha_class_init,
-    },
+        {
+                .name = TYPE_S5L8702_SHA,
+                .parent = TYPE_SYS_BUS_DEVICE,
+                .instance_init = s5l8702_sha_init,
+                .instance_size = sizeof(S5L8702ShaState),
+                .class_init = s5l8702_sha_class_init,
+        },
 };
 DEFINE_TYPES(s5l8702_sha_types);
--- include/hw/misc/s5l8702-sha.h
+++ include/hw/misc/s5l8702-sha.h
@@ -11,15 +11,7 @@ OBJECT_DECLARE_SIMPLE_TYPE(S5L8702ShaState, S5L8702_SHA)
 #define S5L8702_SHA_BASE    0x38000000
 #define S5L8702_SHA_SIZE    0x00100000
 
-#define S5L8702_SHA_NUM_REGS    (S5L8702_SHA_SIZE / sizeof(uint32_t))
-
 #define SHA1_BUFFER_SIZE 1024 * 1024
-
-#define SHA_CONFIG         0x0
-#define SHA_RESET          0x4
-#define SHA_MEMORY_MODE    0x80 // whether we read from the memory
-#define SHA_MEMORY_START   0x84
-#define SHA_INSIZE         0x8c
 
 struct S5L8702ShaState {
     /*< private >*/
@@ -28,17 +20,14 @@ struct S5L8702ShaState {
     /*< public >*/
     MemoryRegion iomem;
 
-    uint32_t config;
-    uint32_t memory_start;
-    uint32_t memory_mode;
-    uint32_t insize;
     uint8_t buffer[SHA1_BUFFER_SIZE];
-    uint32_t hw_buffer[0x10]; // hardware buffer
-    uint32_t buffer_ind;
-    uint8_t hashout[0x14];
-    bool hw_buffer_dirty;
+    size_t buffer_len;
+
+    uint32_t config;
+    uint32_t inbuf[16];
+
+    uint8_t outbuf[0x14];
     bool hash_computed;
-    SHA_CTX ctx;
 };
 
 #endif /* HW_MISC_S5L8702_SHA_H */
