0d3adbb165b921bd7e4146a63945844e557af391
Iscle <albertiscle9@gmail.com>
2026-05-18 21:35:05 +0200

    Fix swapLong function signature to accept uint64_t directly

--- hw/misc/s5l8702-sha.c
+++ hw/misc/s5l8702-sha.c
@@ -9,8 +9,7 @@
 #define SHA1OUT     0x0020
 #define SHA1IN      0x0040
 
-static uint64_t swapLong(void *X) {
-    uint64_t x = (uint64_t) X;
+static uint64_t swapLong(uint64_t x) {
     x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32;
     x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16;
     x = (x & 0x00FF00FF00FF00FF) << 8 | (x & 0xFF00FF00FF00FF00) >> 8;
